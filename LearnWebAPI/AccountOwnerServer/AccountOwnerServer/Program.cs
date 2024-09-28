789a22d8b5c48e3a5814854d141abbdb70029435
Omkareshwar Nath Dwivedy <omkareshwarnath.dwivedy.sbg@gmail.com>
2024-09-28 19:01:02 +0530

    Enabled CORS, to communicate different domains

--- LearnWebAPI/AccountOwnerServer/AccountOwnerServer/Program.cs
+++ LearnWebAPI/AccountOwnerServer/AccountOwnerServer/Program.cs
@@ -2,11 +2,41 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+// This policy name will be used to allow CORS
+const string POLICY_NAME = "CorsPolicy";
+
+//builder.Services.AddCors(options =>
+//{
+//    options.AddPolicy(name: POLICY_NAME, builder =>
+//    {
+//        builder.AllowAnyOrigin()
+//            .AllowAnyMethod()
+//            .AllowAnyHeader();
+//    });
+//});
+
+// Only single policy is ok, use default one
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(builder =>
+    {
+        builder.AllowAnyOrigin()
+        .AllowAnyHeader()
+        .AllowAnyMethod();
+    });
+});
+
 builder.Services.AddControllers();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+
+// Policy name is passed, this way is prefered when having multiple policies
+// app.UseCors(POLICY_NAME);
+
+// For default cors
+app.UseCors();
 
 app.UseHttpsRedirection();
 
