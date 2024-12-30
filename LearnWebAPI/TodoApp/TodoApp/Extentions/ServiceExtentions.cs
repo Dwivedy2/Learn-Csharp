55de42091155641f0f42b8693ed05049597c3e97
Omkareshwar Nath Dwivedy <omkareshwarnath.dwivedy.sbg@gmail.com>
2024-12-30 17:52:06 +0530

    Swagger setup

---
+++ LearnWebAPI/TodoApp/TodoApp/Extentions/ServiceExtentions.cs
@@ -0,0 +1,19 @@
+﻿namespace TodoApp.Extentions
+{
+    public static class ServiceExtentions
+    {
+        public static void ConfigureSwaggerGen(this IServiceCollection services)
+        {
+            services.AddSwaggerGen();
+        }
+
+        public static void ConfigureSwaggerDoc(this IApplicationBuilder app)
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "TodoApp API");
+            });
+        }
+    }
+}
--- LearnWebAPI/TodoApp/TodoApp/Program.cs
+++ LearnWebAPI/TodoApp/TodoApp/Program.cs
@@ -1,10 +1,15 @@
+using TodoApp.Extentions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.ConfigureSwaggerGen();
 
 var app = builder.Build();
+
+app.ConfigureSwaggerDoc();
 
 // Configure the HTTP request pipeline.
 
